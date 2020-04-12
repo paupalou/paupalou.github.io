acba13b736b248036bc41bf9163220af42033f71
paupalou <paupalou@gmail.com>
2020-04-12 13:04:24 +0200

    initial commit

---
+++ precache-manifest.921f57f4e117d3ca15e97f9142ecf68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9d5a178cb8c927f5812a19ca3a939a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "092c7ebb7bfef55898db",
+    "url": "/static/css/main.e75325c8.chunk.css"
+  },
+  {
+    "revision": "13b20ae4555421c4f669",
+    "url": "/static/js/2.e683d0ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e683d0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092c7ebb7bfef55898db",
+    "url": "/static/js/main.74556ba2.chunk.js"
+  },
+  {
+    "revision": "371341e12d0a69d26f71",
+    "url": "/static/js/runtime-main.8c3905d0.js"
+  }
+]);
